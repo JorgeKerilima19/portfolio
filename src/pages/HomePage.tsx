b1b9ebeea50982cd5f5cb98bef7d0d4b23bb35cf
JorgeMioska <quispechilojorge@gmail.com>
2023-03-31 04:43:15 -0500

    Homepage ready

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,16 @@
+import "./styles/home.css"
+
 export const HomePage = () => {
   return (
-    <section>
-      <h2>Hello World</h2>
-      <h1>I am Jorge Quispe</h1>
-      <p>Self-taught frontend developer seeking a full-stack career</p>
+    <section className="section__homepage">
+      <div className="homepage-container">
+        <h2 className="">Hello World</h2>
+        <h1 >I am Jorge Quispe</h1>
+        <p>Self-taught frontend developer seeking a full-stack career</p>
+      </div>
+      <div className="homepage-container">
+        <img className="homepage__img" src="../../assets/1.jpg" alt="" />
+      </div>
     </section>
   );
 };
