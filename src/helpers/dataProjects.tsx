ba00002e1844ea850bed06353b63650dd78e5cca
JorgeMioska <quispechilojorge@gmail.com>
2023-04-02 00:30:05 -0500

    project list ready to display

---
+++ src/helpers/dataProjects.tsx
@@ -0,0 +1,7 @@
+export const projectList = [
+  {
+    projectName: "Social Rot",
+    projectImage: <svg className="project-card__img"></svg>,
+    projectDescription: "This is a fb Copy",
+  },
+];
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,20 @@
-import "./styles/projects.css"
+import "./styles/projects.css";
+
+import { projectList as data } from "../helpers/dataProjects";
 
 export const Projects = () => {
   return (
     <section className="section__projects">
       <div className="projects-container">
-        <article className="project-card">
-          <img className="project-card__img" alt="Project image" />
-          <h3 className="project-card__title">Project Title</h3>
-          <p className="project-card__description">Project Description</p>
-        </article>
+        {data.map((project) => (
+          <article className="project-card">
+            <>{project.projectImage}</>
+            <h3 className="project-card__title">{project.projectName}</h3>
+            <p className="project-card__description">
+              {project.projectDescription}
+            </p>
+          </article>
+        ))}
       </div>
     </section>
   );
