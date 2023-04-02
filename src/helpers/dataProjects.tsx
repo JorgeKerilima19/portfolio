db010c0b9785467cda091db0502c2088635567aa
JorgeMioska <quispechilojorge@gmail.com>
2023-04-02 00:36:14 -0500

    project List

--- src/helpers/dataProjects.tsx
+++ src/helpers/dataProjects.tsx
@@ -4,4 +4,29 @@ export const projectList = [
     projectImage: <svg className="project-card__img"></svg>,
     projectDescription: "This is a fb Copy",
   },
+  {
+    projectName: "Guess game",
+    projectImage: <svg className="project-card__img"></svg>,
+    projectDescription: "This is a fb Copy",
+  },
+  {
+    projectName: "Html, Css + Javascript Vanilla Portfolio",
+    projectImage: <svg className="project-card__img"></svg>,
+    projectDescription: "This is a fb Copy",
+  },
+  {
+    projectName: "React task manager",
+    projectImage: <svg className="project-card__img"></svg>,
+    projectDescription: "This is a fb Copy",
+  },
+  {
+    projectName: "React trip page",
+    projectImage: <svg className="project-card__img"></svg>,
+    projectDescription: "This is a fb Copy",
+  },
+  {
+    projectName: "React Pokedex",
+    projectImage: <svg className="project-card__img"></svg>,
+    projectDescription: "This is a fb Copy",
+  },
 ];
