dc8b8ec1351742569688eabcf80c7278ab9ccf95
JorgeMioska <quispechilojorge@gmail.com>
2023-04-03 23:36:11 -0500

    skills data

---
+++ src/helpers/dataSkills.tsx
@@ -0,0 +1,59 @@
+export const codingSkills = [
+  {
+    name: "Html5",
+    type: "language",
+    skillSvg: <svg></svg>,
+  },
+  {
+    name: "Css3",
+    type: "language",
+    skillSvg: <svg></svg>,
+  },
+  {
+    type: "language",
+    skillSvg: <svg></svg>,
+  },
+  {
+    name: "Html5",
+    type: "language",
+    skillSvg: <svg></svg>,
+  },
+  {
+    name: "React",
+    type: "library/framework",
+    skillSvg: <svg></svg>,
+  },
+];
+export const designSkills = [
+  {
+    name: "Abode Photoshop",
+    skillSvg: <svg></svg>,
+  },
+  {
+    name: "Abode Illustrator",
+    skillSvg: <svg></svg>,
+  },
+  {
+    name: "Abode XD",
+    skillSvg: <svg></svg>,
+  },
+  {
+    name: "Abode After Effects",
+    skillSvg: <svg></svg>,
+  },
+  {
+    name: "Figma",
+    skillSvg: <svg></svg>,
+  },
+];
+
+export const learningSkills = [
+  {
+    name: "TypeScript",
+    skillSvg: <svg></svg>,
+  },
+  {
+    name: "NodeJs",
+    skillSvg: <svg></svg>,
+  },
+];
