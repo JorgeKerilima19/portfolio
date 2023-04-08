77de414721e21fc71fa4b273a066865c43c81c05
JorgeMioska <quispechilojorge@gmail.com>
2023-04-07 20:03:31 -0500

    modal

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -1,16 +1,10 @@
-export const ProjectModal = ({ project,children }: any) => {
+export const ProjectModal = ({ project, children }: any) => {
   return (
-    <article className="project-card">
-      <>{project.projectImage}</>
-      <h3 className="project-card__title">{project.projectName}</h3>
-      <p className="project-card__description">{project.projectDescription}</p>
-      <div className="project-card__project-links">
-        <button className="project-link__button">
-          <a href={`${project.projectLink}`}>Link</a>
-        </button>
-        <button className="project-link__button">ScreenShots</button>
+    <>
+      <>{children}</>
+      <div className="card-modal open">
+        <h2>xd</h2>
       </div>
-      <div>{children}</div>
-    </article>
+    </>
   );
 };
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,7 +9,14 @@ export const Projects = () => {
       <div className="projects-container">
         {data.map((project) => (
           <ProjectModal project={project}>
-            <h2>Hello world</h2>
+            <article className="project-card">
+              <>{project.projectImage}</>
+              <h3 className="project-card__title">{project.projectName}</h3>
+              <p className="project-card__description">
+                {project.projectDescription}
+              </p>
+              <button className="project-card__button">more</button>
+            </article>
           </ProjectModal>
         ))}
       </div>
