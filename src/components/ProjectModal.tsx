29c4a1c97e6286cbcc61a601deb0f5a41c87c205
JorgeMioska <quispechilojorge@gmail.com>
2023-04-07 20:51:04 -0500

    correct type returns + interface props

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -1,9 +1,31 @@
-export const ProjectModal = ({ project, children }: any) => {
+interface PropsVerification {
+  project: Project;
+  children?: React.ReactNode;
+  open: boolean;
+  handleOpen: React.MouseEventHandler<HTMLButtonElement>;
+}
+interface Project {
+  projectName: string;
+  projectImage: React.ReactElement;
+  projectDescription: string;
+  projectSs?: string;
+  projectLink?: string;
+}
+
+export const ProjectModal = ({
+  project,
+  children,
+  open,
+  handleOpen,
+}: PropsVerification) => {
   return (
     <>
       <>{children}</>
-      <div className="card-modal open">
-        <h2>xd</h2>
+      <div className={`card-modal ${open ? "open" : ""}`}>
+        <section className="card-modal__container">
+          <h3 className="project-card__title">{project.projectName}</h3>
+          <button onClick={handleOpen}>Close</button>
+        </section>
       </div>
     </>
   );
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,21 +1,35 @@
+import { useState } from "react";
+
 import "./styles/projects.css";
 
 import { projectList as data } from "../helpers/dataProjects";
 import { ProjectModal } from "../components";
 
 export const Projects = () => {
+  const [isOpenProject1, setIsOpenProject1] = useState(false);
+
+  const handleClick = (): void => {
+    setIsOpenProject1(!isOpenProject1);
+  };
+
   return (
     <section className="section__projects">
       <div className="projects-container">
         {data.map((project) => (
-          <ProjectModal project={project}>
+          <ProjectModal
+            project={project}
+            open={isOpenProject1}
+            handleOpen={handleClick}
+          >
             <article className="project-card">
               <>{project.projectImage}</>
               <h3 className="project-card__title">{project.projectName}</h3>
               <p className="project-card__description">
                 {project.projectDescription}
               </p>
-              <button className="project-card__button">more</button>
+              <button className="project-card__button" onClick={handleClick}>
+                more
+              </button>
             </article>
           </ProjectModal>
         ))}
