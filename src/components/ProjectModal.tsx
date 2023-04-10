99d176fea0675aa8ac6e28906f8e5891d068111f
JorgeMioska <quispechilojorge@gmail.com>
2023-04-09 20:05:29 -0500

    template

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -27,7 +27,28 @@ export const ProjectModal = ({ project }: PropsVerification) => {
         onClick={() => setOpen(!open)}
       >
         <section className="card-modal__container" onClick={stopPropagation}>
-          <h3 className="project-card__title">{project.projectName}</h3>
+          <div className="modal-project__presentation">
+            <img src="" alt="" />
+            <svg></svg>
+            <h3 className="project-card__title">{project.projectName}</h3>
+          </div>
+          <div className="modal-project__description">
+            <section className="description__container">
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+                facilis magni possimus fuga perspiciatis quis quae tempore
+                quisquam vero maiores.
+              </span>
+            </section>
+            <section className="description__container">
+              <ul>
+                <h4>Built with</h4>
+                <li>JavaScript</li>
+                <li>Html</li>
+                <li>Css</li>
+              </ul>
+            </section>
+          </div>
           <button onClick={() => setOpen(!open)}>Close</button>
         </section>
       </div>
