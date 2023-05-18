b081b5026b8deaf290a7f8835ebdeebb0d433658
JorgeMioska <quispechilojorge@gmail.com>
2023-05-17 23:02:13 -0500

    project section responsiveness

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,17 +16,23 @@ export const About = () => {
         <h3>About me</h3>
         <div className="container">
           <img src="" alt="" />
-          <p>
-            I am a passionate self-taught web developer. My journey started out
-            of curiosity to know how the internet and computers work. Along the
-            way I acquired new technical skills and new ways of dealing with
-            problems as well as enhancing my ability to solve problems.
-          </p>
-          <p>
-            My strongest strength is that I like to face situations that
-            challenges me and my skills, which will gain me more experience to
-            solve future alike problems faster.
-          </p>
+          <div className="about-container">
+            <p>
+              I am a passionate self-taught web developer. My journey started out
+              of curiosity to know how the internet and computers work. Along the
+              way I acquired new technical skills and new ways of dealing with
+              problems as well as enhancing my ability to solve problems.
+            </p>
+            <p>
+              My strongest strength is that I like to face situations that
+              challenges me and my skills, which will gain me more experience to
+              solve future alike problems faster.
+            </p>
+            <p>
+
+            
+            </p>
+          </div>
         </div>
       </div>
       <div>
