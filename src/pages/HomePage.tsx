f707f0fe0a0107d8169291f60d31a38378f0408c
JorgeMioska <quispechilojorge@gmail.com>
2023-05-21 21:49:02 -0500

    homepage colors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
-import "./styles/home.css"
+import "./styles/home.css";
 
 export const HomePage = () => {
   return (
     <section className="section__homepage">
       <div className="homepage-container">
-        <h2 className="">Hello World</h2>
-        <h1 >I am Jorge Quispe</h1>
+        <h2 className="homepage-subtitle">Hello World</h2>
+        <h1 className="homepage-title">
+          I am <span>Jorge</span> Quispe
+        </h1>
         <p>Self-taught frontend developer seeking a full-stack career</p>
       </div>
       <div className="homepage-container">
