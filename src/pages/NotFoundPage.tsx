fc202f5514dd2d95205e6e4b3aaa4a04c0083ab1
JorgeMioska <quispechilojorge@gmail.com>
2023-06-08 23:46:21 -0500

    404 work

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom"
+import "../index.css"
+
+import img from "../../public/404.svg"
 
 export const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <main className="main container gap-2">
+      <h2>404 Error</h2>
+      <h3>Requested page does not exists</h3>  
+      <img style={{width:"50%"}} src={img} alt="" />
+      <Link to="./">
+        <button>Go Back to Page</button>
+      </Link>
+    </main>
   )
 }
