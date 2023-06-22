307dfdf240becd18766d95d27f224b87878b199e
JorgeMioska <quispechilojorge@gmail.com>
2023-06-21 23:26:28 -0500

    404 styles

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,17 +1,18 @@
-import { Link } from "react-router-dom"
-import "../index.css"
+import { Link } from "react-router-dom";
+import "../index.css";
+import "./styles/404.css"
 
-import img from "../../public/404.svg"
+import img from "../../public/404.svg";
 
 export const NotFoundPage = () => {
   return (
-    <main className="main container gap-2">
+    <main className="main container gap-2 section__404">
       <h2>404 Error</h2>
-      <h3>Requested page does not exists</h3>  
-      <img style={{width:"50%"}} src={img} alt="" />
+      <h3>Requested page does not exists</h3>
+      <img style={{ width: "50%" }} src={img} alt="" />
       <Link to="./">
-        <button>Go Back to Page</button>
+        <button className="contact__form__button">Go Back to Page</button>
       </Link>
     </main>
-  )
-}
+  );
+};
