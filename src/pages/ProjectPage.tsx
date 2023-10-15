cd6123a5fa77eb93cc5a2dcb9fd0df4c824a9a66
JorgeMioska <quispechilojorge@gmail.com>
2023-10-14 20:58:15 -0500

    project page working

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,7 +1,19 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Project, projectList } from "../helpers/dataProjects";
 
 export const ProjectPage = () => {
-  const { id } = useParams();
+  const { id }: any = useParams();
 
-  return <div>ProjectPage {id}</div>;
+  const [project, setProject] = useState<Project>();
+
+  useEffect(() => {
+    setProject(projectList[id - 1]);
+  }, []);
+
+  return (
+    <section className="h-screen px-5 py-5">
+      ProjectPage {project?.projectDescription}
+    </section>
+  );
 };
