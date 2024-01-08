45040b2c07a27e01a91b2b9abcd8078384a1e8b5
JorgeMioska <quispechilojorge@gmail.com>
2024-01-08 13:20:49 -0500

    screenshots size reduced + game ss

---
+++ src/helpers/screenshoots/pairGame.ts
@@ -0,0 +1,5 @@
+import ss1 from "../../assets/ScreenShoots/game/game1.png";
+import ss2 from "../../assets/ScreenShoots/game/game2.png";
+import ss3 from "../../assets/ScreenShoots/game/game3.png";
+
+export const gameImages = [ss1, ss2, ss3];
