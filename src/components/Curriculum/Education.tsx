789ece08baeef9e0df4234b06df4b2d3c0f9fafe
JorgeMioska <quispechilojorge@gmail.com>
2024-02-15 20:12:20 -0500

    ...

--- src/components/Curriculum/Education.tsx
+++ src/components/Curriculum/Education.tsx
@@ -1,3 +1,16 @@
 export const Education = () => {
-  return <div>Education</div>;
+  return (
+    <div>
+      <h3>Education</h3>
+      <div>
+        <h4>University of the People</h4>
+        <span>associate software engineering program</span>
+        <span>2022-2024</span>
+      </div>
+      <div>
+        <h4>I.E Miguel Grau Seminario Highschool</h4>
+        <span>GPA: 16/20</span>
+      </div>
+    </div>
+  );
 };
